13d9970314a28f44b2f0a7b45baa6a2df61fc243
Harlan Williams <harlanrhwilliams@gmail.com>
2025-03-26 09:10:36 -0500

    does main break it?

--- matrix.cpp
+++ matrix.cpp
@@ -1,1 +1,41 @@
 #include "matrix.hpp"
+
+Matrix::Matrix(std::size_t N) {
+
+}
+Matrix::Matrix(std::vector<std::vector<int>> nums) {
+
+}
+
+Matrix Matrix::operator+(const Matrix &rhs) const {
+    Matrix m(1);
+    return m;
+}
+Matrix Matrix::operator*(const Matrix &rhs) const {
+    Matrix m(1);
+    return m;
+}
+void Matrix::set_value(std::size_t i, std::size_t j, int n) {
+}
+int Matrix::get_value(std::size_t i, std::size_t j) const {
+    return 0;
+}
+int Matrix::get_size() const {
+    return 0;
+}
+int Matrix::sum_diagonal_major() const {
+    return 0;
+}
+int Matrix::sum_diagonal_minor() const {
+    return 0;
+}
+void Matrix::swap_rows(std::size_t r1, std::size_t r2) {
+}
+void Matrix::swap_cols(std::size_t c1, std::size_t c2) {
+}
+void Matrix::print_matrix() const {
+}
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
